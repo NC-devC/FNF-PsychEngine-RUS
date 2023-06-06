d07322d544a3250a40b43a90972627cb409f6256
NC <keyetpro@gmail.com>
2023-06-06 13:29:36 +0300

    new settings

--- source/ClientPrefs.hx
+++ source/ClientPrefs.hx
@@ -33,6 +33,8 @@ class ClientPrefs {
 	public static var pauseMusic:String = 'Tea Time';
 	public static var checkForUpdates:Bool = true;
 	public static var comboStacking = true;
+
+	public static var language:String = "ru";
 	public static var gameplaySettings:Map<String, Dynamic> = [
 		'scrollspeed' => 1.0,
 		'scrolltype' => 'multiplicative', 
@@ -130,6 +132,8 @@ class ClientPrefs {
 		FlxG.save.data.pauseMusic = pauseMusic;
 		FlxG.save.data.checkForUpdates = checkForUpdates;
 		FlxG.save.data.comboStacking = comboStacking;
+
+		FlxG.save.data.language = language;
 	
 		FlxG.save.flush();
 
@@ -263,6 +267,9 @@ class ClientPrefs {
 		if (FlxG.save.data.checkForUpdates != null)
 		{
 			checkForUpdates = FlxG.save.data.checkForUpdates;
+		}
+		if(FlxG.save.data.language != null) {
+			language = FlxG.save.data.language;
 		}
 		if (FlxG.save.data.comboStacking != null)
 			comboStacking = FlxG.save.data.comboStacking;
---
+++ source/options/LanguageSubState.hx
@@ -0,0 +1,52 @@
+package options;
+
+#if desktop
+import Discord.DiscordClient;
+#end
+import flash.text.TextField;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.addons.display.FlxGridOverlay;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.math.FlxMath;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import lime.utils.Assets;
+import flixel.FlxSubState;
+import flash.text.TextField;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.util.FlxSave;
+import haxe.Json;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxTimer;
+import flixel.input.keyboard.FlxKey;
+import flixel.graphics.FlxGraphic;
+import Controls;
+
+using StringTools;
+
+class LanguageSubState extends BaseOptionsMenu
+{
+	public function new()
+	{
+		title = 'Language Settings';
+		rpcTitle = 'Language Settings Menu'; //for Discord Rich Presence
+
+		var option:Option = new Option('Game language',
+			'Check this if you want to play with\na controller instead of using your Keyboard.',
+			'language',
+			'string',
+			'ru',
+			['ru', 'en']);
+		addOption(option);
+
+		super();
+	}
+
+	function onChangeHitsoundVolume()
+	{
+		FlxG.sound.play(Paths.sound('hitsound'), ClientPrefs.hitsoundVolume);
+	}
+}
