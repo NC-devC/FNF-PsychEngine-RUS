3216eb0b75ed33fe61ef06e51b3835377bf110ac
Ender69 <112335251+ender69funk@users.noreply.github.com>
2023-06-08 17:03:03 +0600

    Update PauseSubState.hx

--- source/PauseSubState.hx
+++ source/PauseSubState.hx
@@ -122,9 +122,6 @@ class PauseSubState extends MusicBeatSubstate
 		add(levelDifficulty);
 
 		
-		switch(ClientPrefs.language)
-		{
-			case 'ru':
 				var blueballedTxt:FlxText = new FlxText(20, 15 + 64, 0, "", 32);
 				blueballedTxt.text = "Пpoигpышeй: " + PlayState.deathCounter;
 				blueballedTxt.scrollFactor.set();
@@ -148,31 +145,7 @@ class PauseSubState extends MusicBeatSubstate
 				chartingText.updateHitbox();
 				chartingText.visible = PlayState.chartingMode;
 				add(chartingText);
-			default:
-				var blueballedTxt:FlxText = new FlxText(20, 15 + 64, 0, "", 32);
-				blueballedTxt.text = "Blueballed: " + PlayState.deathCounter;
-				blueballedTxt.scrollFactor.set();
-				blueballedTxt.setFormat(Paths.font('vcr.ttf'), 32);
-				blueballedTxt.updateHitbox();
-				add(blueballedTxt);
-		
-				practiceText = new FlxText(20, 15 + 101, 0, "PRACTICE MODE", 32);
-				practiceText.scrollFactor.set();
-				practiceText.setFormat(Paths.font('vcr.ttf'), 32);
-				practiceText.x = FlxG.width - (practiceText.width + 20);
-				practiceText.updateHitbox();
-				practiceText.visible = PlayState.instance.practiceMode;
-				add(practiceText);
-		
-				var chartingText:FlxText = new FlxText(20, 15 + 101, 0, "CHARTING MODE", 32);
-				chartingText.scrollFactor.set();
-				chartingText.setFormat(Paths.font('vcr.ttf'), 32);
-				chartingText.x = FlxG.width - (chartingText.width + 20);
-				chartingText.y = FlxG.height - (chartingText.height + 20);
-				chartingText.updateHitbox();
-				chartingText.visible = PlayState.chartingMode;
-				add(chartingText);
-		}
+
 
 		blueballedTxt.alpha = 0;
 		levelDifficulty.alpha = 0;
