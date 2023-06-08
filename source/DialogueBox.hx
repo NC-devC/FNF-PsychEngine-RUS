98d6ed461a75c4cceb03a2d61855df744ee8a471
NC <keyetpro@gmail.com>
2023-06-08 12:29:29 +0300

    SEMPAI

--- source/DialogueBox.hx
+++ source/DialogueBox.hx
@@ -134,12 +134,12 @@ class DialogueBox extends FlxSpriteGroup
 		}
 
 		dropText = new FlxText(242, 502, Std.int(FlxG.width * 0.6), "", 32);
-		dropText.font = 'senpai.ttf';
+		dropText.font = 'pixel.otf';
 		dropText.color = 0xFFD89494;
 		add(dropText);
 
 		swagDialogue = new FlxTypeText(240, 500, Std.int(FlxG.width * 0.6), "", 32);
-		swagDialogue.font = 'senpai.ttf';
+		swagDialogue.font = 'pixel.otf';
 		swagDialogue.color = 0xFF3F2021;
 		swagDialogue.sounds = [FlxG.sound.load(Paths.sound('pixelText'), 0.6)];
 		add(swagDialogue);
